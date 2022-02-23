b1598d6cefa963d7486a3c324286d82e6e4f7f74
vivien41 <88794495+vivien41@users.noreply.github.com>
2022-02-23 15:46:24 +0100

    mise a jour du app

--- backend/app.js
+++ backend/app.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD:app.js
 const express = require('express');
 const mongoose = require('mongoose')
 const bodyParser = require('body-parser');
@@ -11,10 +10,11 @@ const saucesRoutes = require('./routes/sauces');
 
 const app = express();
 
+
 require('dotenv').config();
 
 // mongoose connect
-mongoose.connect(`mongodb+srv://vivienp:lylyvivi0909@cluster0.9jyvr.mongodb.net/P6?retryWrites=true&w=majority`,
+mongoose.connect(`mongodb+srv://vivienp:lylyvivi0505@cluster0.9jyvr.mongodb.net/P6?retryWrites=true&w=majority`,
     { useNewUrlParser: true,
      useUnifiedTopology: true
     })
@@ -29,48 +29,14 @@ app.use((req, res, next) => {
     next();
 });
 
+// Gestion des static (images)
+app.use('/images', express.static(path.join(__dirname, 'images')));
 
+app.use(express.json())
 app.use(bodyParser.json()); 
 
 app.use('/api/auth', userRoutes);
 app.use('/api/sauces', saucesRoutes);
 
 
-=======
-const express = require('express');
-const mongoose = require('mongoose')
-const bodyParser = require('body-parser');
-const path = require('path');
-
-const userRoutes = require('./routes/user');
-
-
-
-
-const app = express();
-
-require('dotenv').config();
-
-// mongoose connect
-mongoose.connect(`mongodb+srv://vivienp:lylyvivi0909@cluster0.9jyvr.mongodb.net/P6?retryWrites=true&w=majority`,
-    { useNewUrlParser: true,
-     useUnifiedTopology: true
-    })
-    .then(() => console.log('Connexion à MongoDB réussie !'))
-    .catch(() => console.log('Connexion à MongoDB échouée !'));
-
-// CORS
-app.use((req, res, next) => {
-    res.setHeader('Access-Control-Allow-Origin', '*');
-    res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization');
-    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
-    next();
-});
-
-
-app.use(bodyParser.json()); 
-
-app.use('/api/auth', userRoutes);
-
->>>>>>> 4805c823928f222d21622c3203cf01b305cc2b6b:backend/app.js
 module.exports = app; 
