7ed4837a6020bdbaf5daaee9843fae5b295c9aa0
vivien41 <88794495+vivien41@users.noreply.github.com>
2022-02-23 15:50:36 +0100

    modification auth

---
+++ authorization.js
@@ -0,0 +1,18 @@
+const jwt = require('jsonwebtoken');
+require('dotenv').config();
+
+module.exports = (req, res, next) => {
+    try {
+        const token = req.headers.Authorization.split(' ')[1]; 
+        const decodedToken = jwt.verify(token, 'RANDOM_TOKEN_SECRET'); 
+        const userId = decodedToken.userId;        
+        if (req.body.userId && req.body.userId !== userId) { 
+            throw 'User id non valable !';
+        } else {
+            next();
+        }
+        next();
+    } catch(error){
+        res.status(401).json({ error: new Error('Invalide requete !')});
+    }
+};
