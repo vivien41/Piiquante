d13e897497208f69ff9c7a866b9809f4dbf60470
vivien41 <88794495+vivien41@users.noreply.github.com>
2022-03-10 16:15:10 +0100

    correction de auth

--- backend/Middleware/authorization.js
+++ backend/Middleware/authorization.js
@@ -1,18 +1,22 @@
-const jwt = require('jsonwebtoken');
-require('dotenv').config();
+const jwt = require("jsonwebtoken");
+require("dotenv").config();
 
 module.exports = (req, res, next) => {
     try {
-        const token = req.headers.Authorization.split(' ')[1]; 
-        const decodedToken = jwt.verify(token, 'RANDOM_TOKEN_SECRET'); 
-        const userId = decodedToken.userId;        
-        if (req.body.userId && req.body.userId !== userId) { 
-            throw 'User id non valable !';
-        } else {
-            next();
-        }
-        next();
-    } catch(error){
-        res.status(401).json({ error: new Error('Invalide requete !')});
+        const token = req.headers.authorization.split(" ")[1]; 
+        const decodedToken = jwt.verify(token, process.env.MONTOKEN); 
+        conseole.log(decodedToken)
+        const userId = decodedToken.userId;
+    /*Ajout de 'userId' à l'objet requete => L'ajout d'un attribut à l'objet req le rend accessible à tout les middleware */
+    req.auth = { userId: userId };
+    /* Vérifier si 'userId' de la requête correspond à celui du token */
+    if (req.body.userId && req.body.userId !== userId) {
+      throw "User ID non valable";
+    } else {
+      next();
     }
+  } 
+  catch (error) {
+    res.status(401).json({ error: error | "Requête non authentifiée" });
+  }
 };
