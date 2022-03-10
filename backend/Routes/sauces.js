2786e238808636e6843b1f4de7fcd41e66e281ab
vivien41 <88794495+vivien41@users.noreply.github.com>
2022-03-10 16:13:24 +0100

    modif  route sauces

--- backend/Routes/sauces.js
+++ backend/Routes/sauces.js
@@ -1,13 +1,16 @@
 const express = require("express");
-const router = express.Router()
-const saucesCtrl = require("../controller/sauces");
-const auth = require('../middleware/authorization'); // middleware qui permet d'authentifier les pages de l'application
+const router = express.Router();
+const sauceCtrl = require("../controller/sauces");
+const auth = require("../middleware/authorization"); // middleware qui permet d'authentifier les pages de l'application
 const multer = require("../middleware/multer");
 
-router.post("/", multer, saucesCtrl.createSauces);
-router.put("/:id", auth, saucesCtrl.modifySauces);
-router.delete("/:id", auth, saucesCtrl.deleteSauces);
-router.get("/", auth, saucesCtrl.getAllSauces);
-router.get("/:id", auth, saucesCtrl.getOneSauces);
+router.post("/", auth, multer, sauceCtrl.createSauce);
+router.put("/:id",  auth, multer, sauceCtrl.modifySauce);
+router.delete("/:id", auth, sauceCtrl.deleteSauce);
+router.get("/", sauceCtrl.getAllSauce);
+router.get("/:id", sauceCtrl.getOneSauce);
+
+//liker une sauce
+router.post("/:id/like",  auth, multer, sauceCtrl.likeSauce);
 
 module.exports = router;
