f1a58f78d354cd78d7d761e736a59479f00141b3
vivien41 <88794495+vivien41@users.noreply.github.com>
2022-03-10 16:12:43 +0100

    modif du user

--- backend/Routes/user.js
+++ backend/Routes/user.js
@@ -2,8 +2,9 @@ const express = require("express");
 const router = express.Router()
 const userCtrl = require("../controller/user");
 
+
 router.post("/signup", userCtrl.signup);
-router.post("/login", userCtrl.login);
+router.post("/login",  userCtrl.login);
 
 
 module.exports = router;
