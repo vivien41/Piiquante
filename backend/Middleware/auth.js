18bc0d8ef1f699f12b2d36a83c95151670edb08f
vivien41 <88794495+vivien41@users.noreply.github.com>
2022-03-21 20:43:00 +0100

    correction du token

---
+++ backend/Middleware/auth.js
@@ -0,0 +1,22 @@
+const jwt = require("jsonwebtoken");
+require("dotenv").config();
+
+module.exports = (req, res, next) => {
+    try {
+        const token = req.headers.authorization.split(" ")[1]; 
+        const decodedToken = jwt.verify(token, process.env.MONTOKEN); 
+        console.log(decodedToken)
+        const userId = decodedToken.userId;
+    /*Ajout de 'userId' à l'objet requete => L'ajout d'un attribut à l'objet req le rend accessible à tout les middleware */
+    req.auth = { userId: userId };
+    /* Vérifier si 'userId' de la requête correspond à celui du token */
+    if (req.body.userId && req.body.userId !== userId) {
+      throw "User ID non valable";
+    } else {
+      next();
+    }
+  } 
+  catch (error) {
+    res.status(403).json({ error: error | "Requête non authentifiée" });
+  }
+};
