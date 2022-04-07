fd9ed008861db88cc489da2aa8d01143ac22272a
vivien41 <88794495+vivien41@users.noreply.github.com>
2022-04-07 21:56:53 +0200

    mise en place du handleDelete route

--- backend/Routes/sauces.js
+++ backend/Routes/sauces.js
@@ -1,12 +1,13 @@
 const express = require("express");
 const router = express.Router();
 const sauceCtrl = require("../controller/sauces");
-const auth = require("../middleware/authorization"); // middleware qui permet d'authentifier les pages de l'application
+const handleDelete = require("../middleware/handleDelete")
+const auth = require("../middleware/auth"); // middleware qui permet d'authentifier les pages de l'application
 const multer = require("../middleware/multer");
 
 router.post("/", auth, multer, sauceCtrl.createSauce);
-router.put("/:id",  auth, multer, sauceCtrl.modifySauce);
-router.delete("/:id", auth, sauceCtrl.deleteSauce);
+router.put("/:id",  auth, handleDelete, multer, sauceCtrl.modifySauce);
+router.delete("/:id", auth, handleDelete, sauceCtrl.deleteSauce);
 router.get("/", sauceCtrl.getAllSauce);
 router.get("/:id", sauceCtrl.getOneSauce);
 
