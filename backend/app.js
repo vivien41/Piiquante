963098ec3e516dce74288dfdf4b08c48f9b90ea0
vivien41 <88794495+vivien41@users.noreply.github.com>
2022-04-07 21:57:58 +0200

    mise en place du ratelimite

--- backend/app.js
+++ backend/app.js
@@ -3,6 +3,7 @@ const mongoose = require("mongoose");
 const bodyParser = require("body-parser");
 const path = require("path");
 const helmet = require("helmet");
+const rateLimit = require('express-rate-limit')
 
 const userRoutes = require("./routes/user");
 const saucesRoutes = require("./routes/sauces");
@@ -30,11 +31,20 @@ app.use((req, res, next) => {
     next();
 });
 
-// Gestion des static (images)
+/* Limiter envoie de plusieur requête en même temps */
+const  limiter  =  rateLimit ( { 
+	windowMs : 15  *  60  *  1000 , /* 15 minutes */
+	max : 100 ,  /* Limite chaque IP à 100 requêtes par 15 minutes */
+	standardHeaders : true ,  /* Limite de taux de retour info dans les en-têtes */
+	legacyHeaders : false ,  /*- Désactive les en-têtes `X-RateLimit-*` */
+});
+
+    // Gestion des static (images)
 app.use("/images", express.static(path.join(__dirname, "images")));
 
 app.use(express.json())
-app.use(bodyParser.json()); 
+app.use(limiter);
+ 
 app.use(helmet.crossOriginResourcePolicy({ policy: "cross-origin" }));
 
 app.use("/api/auth", userRoutes);
